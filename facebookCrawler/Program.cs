6fb46e7734ba1ddb8183b9731cbb7c2850d9a8d7
buithanhnhan76 <nhanbui37@gmail.com>
2022-05-16 23:55:56 +0700

    update facebook crawler

--- facebookCrawler/Program.cs
+++ facebookCrawler/Program.cs
@@ -34,28 +34,34 @@ namespace facebookCrawler
             // Create file
             System.IO.StreamWriter writer = new System.IO.StreamWriter("C:\\Users\\Nhan Bui\\Documents\\Work\\Crawler\\facebookCrawler\\result\\facebookCrawl.csv", false, System.Text.Encoding.UTF8);
             // Title
-            writer.WriteLine("{0},{1},{2}","Tên user","Link user","Post content");
+            writer.WriteLine("{0},{1},{2},{3},{4}","Tên user","Link user","Post content","Image content 1", "Image content 2");
             // System.Threading.Thread.Sleep(1000);
 
             // Scroll
             IJavaScriptExecutor js = (IJavaScriptExecutor) browser;
-            js.ExecuteScript("window.scrollTo({top: 500,left:0, behavior: 'smooth'})");
-            System.Threading.Thread.Sleep(2000);
-            js.ExecuteScript("window.scrollTo({top: 1000,left:0, behavior: 'smooth'})");
-            System.Threading.Thread.Sleep(2000);
-            js.ExecuteScript("window.scrollTo({top: 1500,left:0, behavior: 'smooth'})");
-            System.Threading.Thread.Sleep(2000);
-            js.ExecuteScript("window.scrollTo({top: 2000,left:0, behavior: 'smooth'})");
-            System.Threading.Thread.Sleep(2000);
             js.ExecuteScript("window.scrollTo({top: 2500,left:0, behavior: 'smooth'})");
             System.Threading.Thread.Sleep(2000);
-            js.ExecuteScript("window.scrollTo({top: 3000,left:0, behavior: 'smooth'})");
-            System.Threading.Thread.Sleep(2000);
-            js.ExecuteScript("window.scrollTo({top: 3500,left:0, behavior: 'smooth'})");
-            System.Threading.Thread.Sleep(2000);
-            js.ExecuteScript("window.scrollTo({top: 4000,left:0, behavior: 'smooth'})");
-            System.Threading.Thread.Sleep(2000);
             js.ExecuteScript("window.scrollTo({top: 4500,left:0, behavior: 'smooth'})");
+            System.Threading.Thread.Sleep(2000);
+            js.ExecuteScript("window.scrollTo({top: 6500,left:0, behavior: 'smooth'})");
+            System.Threading.Thread.Sleep(2000);
+            js.ExecuteScript("window.scrollTo({top: 8500,left:0, behavior: 'smooth'})");
+            System.Threading.Thread.Sleep(2000);
+            js.ExecuteScript("window.scrollTo({top: 10500,left:0, behavior: 'smooth'})");
+            System.Threading.Thread.Sleep(2000);
+            js.ExecuteScript("window.scrollTo({top: 12500,left:0, behavior: 'smooth'})");
+            System.Threading.Thread.Sleep(2000);
+            js.ExecuteScript("window.scrollTo({top: 14500,left:0, behavior: 'smooth'})");
+            System.Threading.Thread.Sleep(2000);
+            js.ExecuteScript("window.scrollTo({top: 16500,left:0, behavior: 'smooth'})");
+            System.Threading.Thread.Sleep(2000);
+            js.ExecuteScript("window.scrollTo({top: 18500,left:0, behavior: 'smooth'})");
+            System.Threading.Thread.Sleep(2000);
+             js.ExecuteScript("window.scrollTo({top: 20500,left:0, behavior: 'smooth'})");
+            System.Threading.Thread.Sleep(2000);
+             js.ExecuteScript("window.scrollTo({top: 22500,left:0, behavior: 'smooth'})");
+            System.Threading.Thread.Sleep(2000);
+             js.ExecuteScript("window.scrollTo({top: 24500,left:0, behavior: 'smooth'})");
             System.Threading.Thread.Sleep(2000);
             
             //Select all content divs
@@ -66,14 +72,14 @@ namespace facebookCrawler
             {
                 string outerHtml = content.GetAttribute("outerHTML");
                 // all crawl data
-                string userName, userLink, postContent;
+                string userName = "", userLink = "", postContent = "";
+                List<string> imgContents = new List<string>();
                 // get all value between "" of href
                 // user name, get string between <strong><span> and </span></strong>
                 try{
                     userName = Regex.Match(outerHtml, "<strong><span>(.*?)</span></strong>").Groups[1].Value;
                 }
                 catch{
-                    userName = "";
                 }
                 // user link
                 try{
@@ -96,12 +102,69 @@ namespace facebookCrawler
                     if(postContent.StartsWith("<a")){
                         postContent = "";
                     }
+                    // replace , with ""
+                    postContent = postContent.Replace(",","");
                 }
                 catch{
                     postContent = "";
                 }
+
+                // images in content post
+                // class is changed continuous
+                // try{
+                //     imageContent1 = Regex.Match(outerHtml,"class=\"i09qtzwb n7fi1qx3 datstx6m pmk7jnqg j9ispegn kr520xx4 k4urcfbm bixrwtb6\" referrerpolicy=\"origin-when-cross-origin\" src=\"(.*?)\">").Groups[1].Value;
+                    // delete amp; in url
+                //     imageContent1 = imageContent1.Replace("amp;","");
+                // }
+                // catch{
+                //     imageContent1 = "";
+                // }
+                // try{
+                        // there are two different class
+                //         imageContent1 = Regex.Match(outerHtml,"class=\"i09qtzwb n7fi1qx3 datstx6m pmk7jnqg j9ispegn kr520xx4 k4urcfbm bixrwtb6\" referrerpolicy=\"origin-when-cross-origin\" src=\"(.*?)\">").Groups[1].Value;
+                //         if(imageContent1 == "")
+                //         try{
+                //             imageContent1 = Regex.Match(outerHtml,"class=\"i09qtzwb n7fi1qx3 datstx6m pmk7jnqg j9ispegn kr520xx4 k4urcfbm\" referrerpolicy=\"origin-when-cross-origin\" src=\"(.*?)\">").Groups[1].Value;
+                //         }
+                //         catch{}
+                //         imageContent1 = imageContent1.Replace("amp;","");
+                // }
+                // catch{
+                // }
+                //  try{
+                        // there are two different class
+                //         imageContent2 = Regex.Match(outerHtml,"class=\"i09qtzwb n7fi1qx3 datstx6m pmk7jnqg j9ispegn kr520xx4 k4urcfbm bixrwtb6\" referrerpolicy=\"origin-when-cross-origin\" src=\"(.*?)\">").Groups[2].Value;
+                //         if(imageContent2 == "")
+                //         try{
+                //             imageContent2 = Regex.Match(outerHtml,"class=\"i09qtzwb n7fi1qx3 datstx6m pmk7jnqg j9ispegn kr520xx4 k4urcfbm\" referrerpolicy=\"origin-when-cross-origin\" src=\"(.*?)\">").Groups[2].Value;
+                //         }
+                //         catch{}
+                //         imageContent2 = imageContent2.Replace("amp;","");
+                // }
+                // catch{
+                // }
+                // try{
+                    var imgs = content.FindElements(By.XPath("//img[@class='i09qtzwb n7fi1qx3 datstx6m pmk7jnqg j9ispegn kr520xx4 k4urcfbm']"));
+                    if(imgs.Count == 0){
+                        try{
+                            imgs = content.FindElements(By.XPath("//img[@class='i09qtzwb n7fi1qx3 datstx6m pmk7jnqg j9ispegn kr520xx4 k4urcfbm bixrwtb6']"));
+                        }
+                        catch{
+
+                        }
+                    }
+                    
+                    foreach (var img in imgs){
+                        var imgOuterHtml = img.GetAttribute("outerHtml");
+                        var imgContent = Regex.Match(imgOuterHtml,"src=\"(.*?)\"").Groups[1].Value;
+                        imgContents.Add(imgContent);
+                    }
+                // }
+                // catch{
+
+                // }
                 // write
-                writer.WriteLine("{0},{1},{2}",userName, userLink, postContent);
+                writer.WriteLine("{0},{1},{2},{3},{4}",userName, userLink, postContent, imgContents[0],imgContents[1]);
                 System.Threading.Thread.Sleep(1000);
             }
           
